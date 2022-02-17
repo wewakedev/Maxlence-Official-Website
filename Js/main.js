9cbd770f2053069e7f4c994acf2fcef18f23b6a3
wewakedevs <vivekrawat566@gmail.com>
2022-02-17 12:13:24 +0530

    designv-1

--- Js/main.js
+++ Js/main.js
@@ -179,14 +179,43 @@ $('.carousel').slick({
   
   function myFunction(id) {
     var checkBox = document.getElementById(id);
-    var text = document.getElementById(id+"_content");
+    var content = document.getElementById(id+"_content");
     if (checkBox.checked == true) {
-      text.style.display = "block";
+      content.style.display = "block";
     } else {
-      text.style.display = "none";
+      content.style.display = "none";
     }
   }
 
+// View all
+
+ $("document").ready(function() {
+            $("#link").click(function(){
+                $a = $(this);
+                var action = $a.data('action');
+                if(action == "all"){
+                    $('.case').attr("checked","true");
+                    $a.text("View less").data("action","none");
+                }
+                if(action == "none"){
+                    $('.case').removeAttr("checked");
+                    $a.text("View all").data("action","all");
+                }
+                updatecount();
+                return false;
+            })
+            $(".case").click(function(){
+               if($(".case").length == $(".case:checked").length){
+                    $("#link").text("None").data("action","none");
+               }else{
+                    $("#link").text("View all").data("action","all");
+               }
+               updatecount();
+            })
+        })
+        var updatecount = function(){
+            $("#count").text($(".case:checked").length);
+        }
   
   let el = $('.switch');
 let cur = el.find('.current');
@@ -241,269 +270,6 @@ $(".dropdown-text").html('(' + total + ') Selected');
 
 
 
-// Contact Form File Upload
-
-// Design By
-// - https://dribbble.com/shots/13992184-File-Uploader-Drag-Drop
-
-// Select Upload-Area
-const uploadArea = document.querySelector('#uploadArea')
-
-// Select Drop-Zoon Area
-const dropZoon = document.querySelector('#dropZoon');
-
-// Loading Text
-const loadingText = document.querySelector('#loadingText');
-
-// Slect File Input 
-const fileInput = document.querySelector('#fileInput');
-
-// Select Preview Image
-const previewImage = document.querySelector('#previewImage');
-
-// File-Details Area
-const fileDetails = document.querySelector('#fileDetails');
-
-// Uploaded File
-const uploadedFile = document.querySelector('#uploadedFile');
-
-// Uploaded File Info
-const uploadedFileInfo = document.querySelector('#uploadedFileInfo');
-
-// Uploaded File  Name
-const uploadedFileName = document.querySelector('.uploaded-file__name');
-
-// Uploaded File Icon
-const uploadedFileIconText = document.querySelector('.uploaded-file__icon-text');
-
-// Uploaded File Counter
-const uploadedFileCounter = document.querySelector('.uploaded-file__counter');
-
-// ToolTip Data
-const toolTipData = document.querySelector('.upload-area__tooltip-data');
-
-// Images Types
-const imagesTypes = [
-  "jpeg",
-  "png",
-  "svg",
-  "gif"
-];
-
-// Append Images Types Array Inisde Tooltip Data
-toolTipData.innerHTML = [...imagesTypes].join(', .');
-
-// When (drop-zoon) has (dragover) Event 
-dropZoon.addEventListener('dragover', function (event) {
-  // Prevent Default Behavior 
-  event.preventDefault();
-
-  // Add Class (drop-zoon--over) On (drop-zoon)
-  dropZoon.classList.add('drop-zoon--over');
-});
-
-// When (drop-zoon) has (dragleave) Event 
-dropZoon.addEventListener('dragleave', function (event) {
-  // Remove Class (drop-zoon--over) from (drop-zoon)
-  dropZoon.classList.remove('drop-zoon--over');
-});
-
-// When (drop-zoon) has (drop) Event 
-dropZoon.addEventListener('drop', function (event) {
-  // Prevent Default Behavior 
-  event.preventDefault();
-
-  // Remove Class (drop-zoon--over) from (drop-zoon)
-  dropZoon.classList.remove('drop-zoon--over');
-
-  // Select The Dropped File
-  const file = event.dataTransfer.files[0];
-
-  // Call Function uploadFile(), And Send To Her The Dropped File :)
-  uploadFile(file);
-});
-
-// When (drop-zoon) has (click) Event 
-dropZoon.addEventListener('click', function (event) {
-  // Click The (fileInput)
-  fileInput.click();
-});
-
-// When (fileInput) has (change) Event 
-fileInput.addEventListener('change', function (event) {
-  // Select The Chosen File
-  const file = event.target.files[0];
-
-  // Call Function uploadFile(), And Send To Her The Chosen File :)
-  uploadFile(file);
-});
-
-// Upload File Function
-function uploadFile(file) {
-  // FileReader()
-  const fileReader = new FileReader();
-  // File Type 
-  const fileType = file.type;
-  // File Size 
-  const fileSize = file.size;
-
-  // If File Is Passed from the (File Validation) Function
-  if (fileValidate(fileType, fileSize)) {
-    // Add Class (drop-zoon--Uploaded) on (drop-zoon)
-    dropZoon.classList.add('drop-zoon--Uploaded');
-
-    // Show Loading-text
-    loadingText.style.display = "block";
-    // Hide Preview Image
-    previewImage.style.display = 'none';
-
-    // Remove Class (uploaded-file--open) From (uploadedFile)
-    uploadedFile.classList.remove('uploaded-file--open');
-    // Remove Class (uploaded-file__info--active) from (uploadedFileInfo)
-    uploadedFileInfo.classList.remove('uploaded-file__info--active');
-
-    // After File Reader Loaded 
-    fileReader.addEventListener('load', function () {
-      // After Half Second 
-      setTimeout(function () {
-        // Add Class (upload-area--open) On (uploadArea)
-        uploadArea.classList.add('upload-area--open');
-
-        // Hide Loading-text (please-wait) Element
-        loadingText.style.display = "none";
-        // Show Preview Image
-        previewImage.style.display = 'block';
-
-        // Add Class (file-details--open) On (fileDetails)
-        fileDetails.classList.add('file-details--open');
-        // Add Class (uploaded-file--open) On (uploadedFile)
-        uploadedFile.classList.add('uploaded-file--open');
-        // Add Class (uploaded-file__info--active) On (uploadedFileInfo)
-        uploadedFileInfo.classList.add('uploaded-file__info--active');
-      }, 500); // 0.5s
-
-      // Add The (fileReader) Result Inside (previewImage) Source
-      previewImage.setAttribute('src', fileReader.result);
-
-      // Add File Name Inside Uploaded File Name
-      uploadedFileName.innerHTML = file.name;
-
-      // Call Function progressMove();
-      progressMove();
-    });
-
-    // Read (file) As Data Url 
-    fileReader.readAsDataURL(file);
-  } else { // Else
-
-    this; // (this) Represent The fileValidate(fileType, fileSize) Function
-
-  };
-};
-
-// Progress Counter Increase Function
-function progressMove() {
-  // Counter Start
-  let counter = 0;
-
-  // After 600ms 
-  setTimeout(() => {
-    // Every 100ms
-    let counterIncrease = setInterval(() => {
-      // If (counter) is equle 100 
-      if (counter === 100) {
-        // Stop (Counter Increase)
-        clearInterval(counterIncrease);
-      } else { // Else
-        // plus 10 on counter
-        counter = counter + 10;
-        // add (counter) vlaue inisde (uploadedFileCounter)
-        uploadedFileCounter.innerHTML = `${counter}%`
-      }
-    }, 100);
-  }, 600);
-};
-
-
-// Simple File Validate Function
-function fileValidate(fileType, fileSize) {
-  // File Type Validation
-  let isImage = imagesTypes.filter((type) => fileType.indexOf(`image/${type}`) !== -1);
-
-  // If The Uploaded File Type Is 'jpeg'
-  if (isImage[0] === 'jpeg') {
-    // Add Inisde (uploadedFileIconText) The (jpg) Value
-    uploadedFileIconText.innerHTML = 'jpg';
-  } else { // else
-    // Add Inisde (uploadedFileIconText) The Uploaded File Type 
-    uploadedFileIconText.innerHTML = isImage[0];
-  };
-
-  // If The Uploaded File Is An Image
-  if (isImage.length !== 0) {
-    // Check, If File Size Is 2MB or Less
-    if (fileSize <= 2000000) { // 2MB :)
-      return true;
-    } else { // Else File Size
-      return alert('Please Your File Should be 2 Megabytes or Less');
-    };
-  } else { // Else File Type 
-    return alert('Please make sure to upload An Image File Type');
-  };
-};
-
-// :)
-
-// Close language dropdown panel
-
-options.on('click', function(e) {
-  e.stopPropagation();
-  el.removeClass('anim-options');
-  el.removeClass('show-shadow');
-  
-  let newLang = $(this).data('lang');
-  
-  cur.find('span').text(newLang);
-  content.attr('class', newLang);
-  
-  setLang(newLang);
-  
-  options.removeClass('selected');
-  $(this).addClass('selected');
-  
-  setTimeout(function() {
-    el.removeClass('show-options');
-  }, 600);
-});
-
-
-// Save selected options into Local Storage
-
-function getLang() {
-  let lang;
-  if (localStorage.getItem('currentLang') === null) {
-    lang = cur.find('span').text();
-  } else {
-    lang = JSON.parse(localStorage.getItem('currentLang')).toLowerCase();
-  }
-  
-  // console.log(lang);
-
-  cur.find('span').text(lang);
-  options.parent().find(`li[data-lang="${lang}"]`).addClass('selected');
-  
-  content.attr('class', lang);
-}
-
-getLang();
-
-function setLang(newLang) {
-    localStorage.setItem('currentLang', JSON.stringify(newLang).toLowerCase());
-  
-  content.attr('class', newLang);
-  
-  // console.log('New language is: ' + newLang);
-}
 
 
 
