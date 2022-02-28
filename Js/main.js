6765e5a4f679c902f58b708a8db01427fc6c3d61
wewakedevs <vivekrawat566@gmail.com>
2022-02-28 16:23:40 +0530

    Seomainpage

--- Js/main.js
+++ Js/main.js
@@ -269,10 +269,29 @@ $(".dropdown-text").html('(' + total + ') Selected');
 });
 
 
+// Load more/ LEss
+$(document).ready(function(){
 
+  var list = $(".list .featcamp");
+  var numToShow = 4;
+  var button = $("#next");
+  var numInList = list.length;
+  list.hide();
+  if (numInList > numToShow) {
+    button.show();
+  }
+  list.slice(0, numToShow).show();
 
+  button.click(function(){
+      var showing = list.filter(':visible').length;
+      list.slice(showing - 1, showing + numToShow).fadeIn();
+      var nowShowing = list.filter(':visible').length;
+      if (nowShowing >= numInList) {
+        button.hide();
+      }
+  });
 
-
+});
 ///Question feed 
 
 $(function() {
